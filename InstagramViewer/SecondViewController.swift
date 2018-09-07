a76c9d302ce9a617964c6b7c1013cf40fb5affd2
MujiLeica <chongl4@student.unimelb.edu.au>
2018-09-08 01:45:51 +1000

    Initial Commit

---
+++ InstagramViewer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  InstagramViewer
+//
+//  Created by CHONG LIU on 8/9/18.
+//  Copyright © 2018 CHONG LIU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ InstagramViewerTests/InstagramViewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  InstagramViewerTests.swift
+//  InstagramViewerTests
+//
+//  Created by CHONG LIU on 8/9/18.
+//  Copyright © 2018 CHONG LIU. All rights reserved.
+//
+
+import XCTest
+@testable import InstagramViewer
+
+class InstagramViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
