8e58fea884ad6679b020f6915a39762ee36a68f2
MujiLeica <chongl4@student.unimelb.edu.au>
2018-09-20 23:33:39 +1000

    Add CameraViewController, Image Picker, Crop Functions and Partially implement upload

--- InstagramViewer/CameraViewController.swift
+++ InstagramViewer/CameraViewController.swift
@@ -7,29 +7,102 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseStorage
+import CropViewController
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, CropViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var shareButton: UIButton!
+    var selectedImage: UIImage?
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.photoClick))
+        photo.addGestureRecognizer(tapGesture)
+        photo.isUserInteractionEnabled = true
     }
-    */
 
+    @objc func photoClick() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a Source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera)
+            {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            else
+            {
+                print("Camera not available")
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Liabrary", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            presentCropViewController(input: image)
+        }
+        //dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    @IBAction func shareButtonClicked(_ sender: Any) {
+        if let uploadPhoto = self.selectedImage, let imageData = UIImageJPEGRepresentation(uploadPhoto, 0.1) {
+            let photoIDString = NSUUID().uuidString
+            let storageRef = Storage.storage().reference(forURL: "gs://comp90018instagramviewer.appspot.com").child("posts").child(photoIDString)
+            storageRef.putData(imageData, metadata: nil, completion: { (uploadMetadata, error) in
+                if error != nil {
+                    return
+                }
+                print("success")
+                //let photoURL = uploadMetadata.
+            })
+        }
+    }
+    
+    
+    
+    
+    
+    //TOCropViewController
+    func presentCropViewController(input: UIImage) {
+        let image: UIImage = input //Load an image
+        let cropViewController = CropViewController(image: image)
+        cropViewController.delegate = self
+        let top = topMostController()
+        top.present(cropViewController, animated: true, completion: nil)
+    }
+    
+    func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        // 'image' is the newly cropped version of the original image
+        selectedImage = image
+        photo.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func topMostController() -> UIViewController {
+        var topController: UIViewController = UIApplication.shared.keyWindow!.rootViewController!
+        while (topController.presentedViewController != nil) {
+            topController = topController.presentedViewController!
+        }
+        return topController
+    }
 }
+
