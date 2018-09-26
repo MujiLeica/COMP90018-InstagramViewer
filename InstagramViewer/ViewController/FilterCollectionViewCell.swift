b335e14118283cacec80b300d07a9abbdcae9de9
Elsie <wing29640464@qq.com>
2018-09-26 15:54:23 +1000

    merge every changes until now
    no bugs

---
+++ InstagramViewer/ViewController/FilterCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionViewCell.swift
+//  InstagramViewer
+//
+//  Created by Huiqun Chen on 26/9/18.
+//  Copyright © 2018 CHONG LIU. All rights reserved.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+    
+}
