ec31208e5e286ab987adb8ea7059f7d5e884c797
Elsie <wing29640464@qq.com>
2018-10-18 17:57:00 +1100

    comment function(not finished part3)

--- InstagramViewer/ViewController/CommentViewController.swift
+++ InstagramViewer/ViewController/CommentViewController.swift
@@ -13,13 +13,40 @@ import FirebaseStorage
 
 class CommentViewController: UIViewController {
 
+    var comments = [Comment]()
+    var users = [User]()
+    let postID = NSUUID().uuidString
     @IBOutlet weak var sendButtonControl: UIButton!
     @IBOutlet weak var commentTextField: UITextField!
     
+    @IBOutlet weak var bottonConstrain: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         sendButtonControl.isEnabled = false
         handleTextField()
+        loadComments()
+        }
+    
+   
+    
+    func loadComments()
+    {
+        let postCommentRef = Database.database().reference(fromURL: "https://comp90018instagramviewer.firebaseio.com/").child("post-comments").child(self.postID)
+        postCommentRef.observe(.childAdded, with: {
+            snapshot in
+            print("Observe Key")
+            print(snapshot.key)
+            Database.database().reference(fromURL: "https://comp90018instagramviewer.firebaseio.com/").child("Comments").child(snapshot.key).observeSingleEvent(of: .value, with: {
+                snapshotComments in
+                if let dict = snapshotComments.value as? [String: Any] {
+                    let newComment = Comment.transformComment(dict: dict)
+                    self.fetchUser(uid: newComment.uid!, completed: {
+                        self.comments.append(newComment)
+                        self.tableView.reloadData()
+                    })
+                }
+            })
+        })
     }
     
     func handleTextField() {
@@ -45,9 +72,11 @@ class CommentViewController: UIViewController {
             let newCommentReference = commentReference.child(commentID)
             let currentUser = Auth.auth().currentUser?.uid
             newCommentReference.setValue(["UserID":  currentUser!, "CommentText": commentTextField.text!])
-            
-
+            let postCommentRef = Database.database().reference(fromURL: "https://comp90018instagramviewer.firebaseio.com/").child("post-comments").child(postID).child(commentID)
+            postCommentRef.setValue(true)
     }
+
+
     
 
     
